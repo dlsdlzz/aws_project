53c692999dd5deeab32cb5c87d203a31ce8c17c0
dlsdlzz <dlsdlzz@naver.com>
2023-05-23 16:12:28 +0900

    20230523

--- 01-Terraform/one-webserver/main.tf
+++ 01-Terraform/one-webserver/main.tf
@@ -3,33 +3,38 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-	ami = "ami-0c6e5afdd23291f73"
-	instance_type = "t2.micro"
-	vpc_security_group_ids= [aws_security_group.instance.id]
+  ami                    = "ami-0c6e5afdd23291f73"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
-	user_data = <<-EOF
+  user_data = <<-EOF
 							#! /bin/bash
 							echo "Hello World" > index.html
-							nohup busybox httpd -f -p 8080 &
+							nohup busybox httpd -f -p ${var.server_port} &
 							EOF
 
-	tags = {
-		Name = "aws03-example"
-	}
+  tags = {
+    Name = "aws03-example"
+  }
 }
 
 resource "aws_security_group" "instance" {
-	name = "aws03-terraform-example-instance"
+  name = "aws03-terraform-example-instance"
 
-	ingress {
-		from_port = 8080
-		to_port = 8080
-		protocol = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 output "public_ip" {
-	value = aws_instance.example.public_ip
-	description = "The pulbic IP of the Example Instance"
+  value       = aws_instance.example.public_ip
+  description = "The pulbic IP of the Example Instance"
+}
+
+variable "server_port" {
+  description = "The port will use for HTTP requests"
+  type        = number
 }
