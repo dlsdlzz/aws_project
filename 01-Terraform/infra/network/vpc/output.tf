ff931d75ffbce364359dab52f3bfb4bdc3ba76e4
dlsdlzz <dlsdlzz@naver.com>
2023-06-20 16:09:35 +0900

    20230620

---
+++ 01-Terraform/infra/network/vpc/output.tf
@@ -0,0 +1,4 @@
+output "vpc_id" {
+    value = aws_vpc.aws03_vpc.id
+}
+
---
+++ 01-Terraform/infra/network/vpc/variable.tf
@@ -0,0 +1,17 @@
+variable "vpc_cidr" {
+    default = "10.3.0.0/16"
+}
+
+variable "public_subnet" {
+    type = list
+    default = ["10.3.0.0/24", "10.3.16.0/24"]
+}
+variable "private_subnet" {
+    type = list
+    default = ["10.3.64.0/24", "10.3.80.0/24"]
+}
+
+variable "azs" {
+    type = list
+    default = ["ap-northeast-2a","ap-northeast-2c"]
+}
